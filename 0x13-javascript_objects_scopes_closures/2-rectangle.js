75412f70f3ad4a5258ee5d84a8cf5118447667a8
DanVillalpando <danielvch5@gmail.com>
2022-08-02 19:25:15 -0700

    1 & 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 /* empty class Rectangle that defines a rectangle */
 class Rectangle {
   constructor (w, h) {
-    if (((w = parseInt(w)) || (h = parseInt(h))) > 0) {
+    if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
       this.width = w;
       this.height = h;
     }
