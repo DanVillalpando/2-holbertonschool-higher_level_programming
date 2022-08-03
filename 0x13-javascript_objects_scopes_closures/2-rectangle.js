0e29a9da2960fe52a753c798a3a5e38eb30f8b4e
DanVillalpando <danielvch5@gmail.com>
2022-08-02 19:20:46 -0700

    1 & 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /* empty class Rectangle that defines a rectangle */
 class Rectangle {
-    constructor (w, h) {
-	if (((w = parseInt(w)) || (h = parseInt(h))) > 0) {
-	    this.width = w;
-	    this.height = h;
-	}
+  constructor (w, h) {
+    if (((w = parseInt(w)) || (h = parseInt(h))) > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
